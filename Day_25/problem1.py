0e52923aaec75b097527a629dd5f2b92dd79b968
Asbaa Thakur <asbaa.thakur@s.amity.edu>
2025-09-16 13:09:50 +0530

    asd

---
+++ Day_25/problem1.py
@@ -0,0 +1,61 @@
+"""
+1935. Maximum Number of Words You Can Type
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+
+Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+
+ 
+
+Example 1:
+
+Input: text = "hello world", brokenLetters = "ad"
+Output: 1
+Explanation: We cannot type "world" because the 'd' key is broken.
+Example 2:
+
+Input: text = "leet code", brokenLetters = "lt"
+Output: 1
+Explanation: We cannot type "leet" because the 'l' and 't' keys are broken.
+Example 3:
+
+Input: text = "leet code", brokenLetters = "e"
+Output: 0
+Explanation: We cannot type either word because the 'e' key is broken.
+
+"""
+
+
+class Solution(object):
+    def canBeTypedWords(self, text, brokenLetters):
+        """
+        :type text: str
+        :type brokenLetters: str
+        :rtype: int
+        """
+        na = 0
+        word = ""
+        list1 = []
+        for c in text:
+            if c != " ":
+                word += c   
+            else:
+                
+                if any(b in word for b in brokenLetters):
+                    list1.append(word)
+                else:
+                    na += 1
+                word = "" 
+        
+        
+        if word:
+            if any(b in word for b in brokenLetters):
+                list1.append(word)
+            else:
+                na += 1
+        return na
